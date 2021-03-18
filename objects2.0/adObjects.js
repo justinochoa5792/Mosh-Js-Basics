85dbada65c63853ee51683914c856a73864f393c
Justin Ochoa <just.och92@gmail.com>
2021-03-18 19:22:58 -0400

    finished object section

--- objects2.0/adObjects.js
+++ objects2.0/adObjects.js
@@ -18,9 +18,31 @@ console.log(circle.draw);
 
 function Circle(radius) {
   this.radius = radius;
+
+  let defaultLocation = { x: 0, y: 0 };
+  this.getDefaultLocation = function () {
+    return defaultLocation;
+  };
+
   this.draw = function () {
     console.log("draw");
   };
+
+  Object.defineProperty(this, "defaultLocation", {
+    get: function () {
+      return defaultLocation;
+    },
+  });
 }
 const another = new Circle(1);
+console.log(another);
+
+for (let key in another) {
+  console.log(key);
+}
+
+if ("radius" in another) {
+  console.log("circle has a radius");
+}
+
 console.log(another);
